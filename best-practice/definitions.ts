5c7ed6bdfb1539b082ccc3219867f0aaeaa15b91
akash1810 <akash1810@users.noreply.github.com>
2023-06-23 13:15:03 +0100

    Add some GitHub best practices
    Tabulating the information on https://github.com/guardian/recommendations/blob/main/github.md for easy referencing.

--- best-practice/definitions.ts
+++ best-practice/definitions.ts
@@ -9,6 +9,57 @@ const github: readonly IBestPractice[] = [
     howToCheck: "Manual. View the repository on https://github.com",
     howToExempt: "N/A",
   },
+  {
+    name: "Branch Protection",
+    owner: "[@guardian](https://github.com/orgs/guardian/teams/all)",
+    description:
+      "Enable branch protection for the default branch, ensuring changes are reviewed before being deployed.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt: "N/A",
+  },
+  {
+    name: "Access",
+    owner: "[@guardian](https://github.com/orgs/guardian/teams/all)",
+    description:
+      "Grant access on a team basis, rather than directly to individuals.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt: "N/A",
+  },
+  {
+    name: "Collaborators",
+    owner: "[@guardian](https://github.com/orgs/guardian/teams/all)",
+    description:
+      "Include at least one GitHub team as a collaborator with admin access.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt:
+      "Repositories *without* the one of following topics are exempt: production, testing, documentation",
+  },
+  {
+    name: "Archiving",
+    owner:
+      "[DevX Operations](https://github.com/orgs/guardian/teams/devx-operations)",
+    description: "Repositories that are no longer used should be archived.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt: "N/A",
+  },
+  {
+    name: "Topics",
+    owner:
+      "[DevX Security](https://github.com/orgs/guardian/teams/devx-security)",
+    description:
+      "Repositories should have a topic, to help understand what is in production.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt: "Repositories owned *only* by non-P&E teams are exempt.",
+  },
+  {
+    name: "Contents",
+    owner:
+      "[DevX Security](https://github.com/orgs/guardian/teams/devx-security)",
+    description:
+      "Never commit secret information. Avoid private information in public repositories.",
+    howToCheck: "Manual. View the repository on https://github.com",
+    howToExempt: "N/A",
+  },
 ] as const satisfies readonly IBestPractice[];
 
 const aws: readonly IBestPractice[] = [
