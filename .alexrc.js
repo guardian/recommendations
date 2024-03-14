103772778bd20dfec3d4746b88e19399001639ca
Chris Jones <chrislomaxjones96@gmail.com>
2024-03-14 10:18:54 +0000

    Add actor to alex allowlist

--- .alexrc.js
+++ .alexrc.js
@@ -1,5 +1,6 @@
 exports.profanitySureness = 1;
 exports.allow = [
-    "steward-stewardess", //Exclude this rule as we get false positives from references to Scala Steward
-    "special"
+  "steward-stewardess", //Exclude this rule as we get false positives from references to Scala Steward
+  "special",
+  "actor-actress", // Used in Security, as in "threat actor"
 ];
